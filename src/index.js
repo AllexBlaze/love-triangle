1b4c80bd8a832ab1b8c7715d97b30e2aecfcb2a2
Alexander Naumchick <naum4ick@yandex.ru>
2018-09-13 21:03:31 +0300

    add

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let length = preferences.length;
+  let SpichCounter = 0;
+
+  for (let i = 0; i < length; i++) {
+    if(preferences[i] == i+1) continue;
+    if(preferences[preferences[preferences[i] - 1] - 1] === i + 1) SpichCounter++;
+  }
+  return SpichCounter / 3;
 };
